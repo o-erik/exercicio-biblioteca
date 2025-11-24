daaf882b162c3239fec2bc410984d142d0d0f366
o-erik <erikmartins.pgm@gmail.com>
2025-11-24 10:22:51 -0300

    commit inicial

---
+++ src/main/java/model/Livro.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(int id, String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+}
